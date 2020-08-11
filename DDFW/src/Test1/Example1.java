8078f0c0e6cdfd7e717550134a600cade8038078
saisusheel <chiliverusaisusheel@gmail.com>
2020-08-11 17:28:46 +0530

    First Commit

---
+++ DDFW/src/Test1/Example1.java
@@ -0,0 +1,93 @@
+package Test1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Example1 
+{
+	
+  
+	public static void main(String[] args) throws IOException, InterruptedException
+	{
+		try {
+			
+		
+		FileInputStream fis = new FileInputStream("C:\\Users\\Rashmitha\\Desktop\\Demo1.xlsx");
+		Thread.sleep(500);
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+	
+		XSSFSheet s= wb.getSheet("Sheet2");
+		
+		int rc = s.getLastRowNum();
+
+		for (int i = 0; i < rc ; i++)
+		{
+		
+			XSSFRow r = s.getRow(i);
+			
+			Thread.sleep(500);
+			int cc = r.getLastCellNum();
+			
+		for (int j = 0; j < cc; j++)
+		
+		 {
+		   XSSFCell c = r.getCell(j);
+		   
+		   String  str = c.getStringCellValue();
+		   
+		   
+		   if (r != null) 
+			{
+				switch (c.getCellType()) 
+				{
+				case Cell.CELL_TYPE_STRING:
+					
+					str = c.getStringCellValue();
+					
+					System.out.println(str);
+					
+					break;
+					
+				case Cell.CELL_TYPE_NUMERIC:
+					
+					str = NumberToTextConverter.toText(c.getNumericCellValue());
+					
+					System.out.println(str);
+					
+					break;
+			   
+		        }
+	       }
+		
+				
+		}
+
+	}
+		}	
+		
+		catch (Exception e) 
+		{
+		
+		System.out.println("File not found");
+          e.printStackTrace();
+
+		}
+			
+		 
+	}
+}
+	
+
+
+	
+	
+
